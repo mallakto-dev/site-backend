3985c676112507b38e6649ca2e089d97442742f9
sergey-royt <goodstop687@gmail.com>
2024-10-05 16:50:04 +0300

    add item photos urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app.goods.views import CategoryViewSet, GoodViewSet
 from app.orders.views import OrderViewSet
